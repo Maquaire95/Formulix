09dbf4861a429a233eff80f06522af815469c8c2
emaquiare <matthieu.080405@gmail.com>
2025-03-24 17:42:22 +0100

    Maj stats

--- Front/js/Statistique.js
+++ Front/js/Statistique.js
@@ -1,20 +1,20 @@
 const sections = document.querySelectorAll('.section');
-let options = {
-    root: null,
-    threshold: 0.5,
-};
+        let options = {
+            root: null,
+            threshold: 0.5,
+        };
 
-let observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.style.opacity = '1';
-        } else {
-            entry.target.style.opacity = '0';
-        }
-    });
-}, options);
+        let observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.style.opacity = '1';
+                } else {
+                    entry.target.style.opacity = '0';
+                }
+            });
+        }, options);
 
-sections.forEach(section => {
-    section.style.opacity = '0';
-    observer.observe(section);
-});
+        sections.forEach(section => {
+            section.style.opacity = '0';
+            observer.observe(section);
+        });
